94abb37df6538180f32a8e90419996254a6d83a5
ShayneLin <linjankangshun@163.com>
2019-12-12 22:31:47 +0800

    初始化web toolkit
    初始化web toolkit

---
+++ src/main/java/com/toolkit/webtoolkit/WebToolkitApplication.java
@@ -0,0 +1,13 @@
+package com.toolkit.webtoolkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebToolkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebToolkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolkit/webtoolkit/WebToolkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolkit.webtoolkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebToolkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
